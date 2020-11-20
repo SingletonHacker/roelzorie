981ff35f8d3f852b07c969ad534c5beb484de474
Bjorn Beek, van der <bjorn.van.der.beek@vecozo.nl>
2020-11-20 21:38:17 +0100

    demo app om te leren om te gaan met conditionals

---
+++ TextToCsv/TextToCsv/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TextToCsv
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var food in args)
+            {
+                var foodLwr = food.ToLower();
+
+                if (foodLwr == "kaas" || foodLwr == "worst" || foodLwr == "oude kaas")
+                {
+                    Console.WriteLine($"Hallo allemaal, {foodLwr} met mosterd");
+                }
+                else if (foodLwr == "komkommer" || foodLwr == "paprika" || foodLwr == "oude kaas")
+                {
+                    Console.WriteLine($"Hallo allemaal, {foodLwr} heksenkaas");
+                }
+                else
+                {
+                    Console.WriteLine($"{foodLwr} zonder mosterd en heksenkaas");
+                }
+            }
+        }
+    }
+}
